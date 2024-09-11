2c5aaa6b7a543c0061d85958718c15fced1993c5
Enea Kuca <eneakuca0@gmail.com>
2024-09-11 20:48:08 +0200

    Small changes

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -6,7 +6,7 @@ const Loader = () => {
     
     <div className="absolute top-0 -left-9 sm:-left-10 md:-left-12 lg:-left-16 h-full flex flex-col justify-center items-center">
        <CircularProgress color="inherit" /> 
-       <div className="w-[20vw] sm:w-[5vw] flex items-center justify-center mt-3">Loading ...</div>
+       <div className="w-[20vw] sm:w-[5vw] flex items-center justify-center mt-3">Loading ... </div>
         
     </div>
   </Html>
