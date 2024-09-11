55f76825eeceb8e1543d63b63c814963db51c259
Enea Kuca <eneakuca0@gmail.com>
2024-09-11 21:05:39 +0200

    Updating UI 2

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -27,7 +27,7 @@ const Footer = () => {
                 </p>
                 <div className="flex">
                     {footerLinks.map((link, i) => (
-                        <p key={link} className="font-semibold text-gray text-xs">
+                        <p key={link} className="font-semibold text-gray text-[10px] sm:text-xs">
                             {link}{' '}
                             {i !== footerLinks.length - 1 && (<span className="mx-1">|</span>)}
                         </p>
